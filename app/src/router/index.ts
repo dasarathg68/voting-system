f1f200da665fce0b2013b6b556a82db1bc06fd4a
Dasarath G <dasarathg68@gmail.com>
2024-05-06 16:01:04 +0530

    chore: cleanup unused files

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import BallotsView from '@/views/BallotsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/ballots',
+      name: 'ballots',
+      component: BallotsView
     }
   ]
 })
