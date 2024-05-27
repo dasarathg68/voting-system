022c776d941484d71b6e6d2ac80893d63f95edb1
Dasarath G <dasarathg68@gmail.com>
2024-05-27 13:59:08 +0530

    feat: sync

--- contract/scripts/deployVoting.js
+++ contract/scripts/deployVoting.js
@@ -6,7 +6,7 @@ async function main() {
     "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
     "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
   ];
-
+  //comment
   const Voting = await VotingContractFactory.deploy();
   console.log("Contract deployed to address:", Voting.address);
 }
